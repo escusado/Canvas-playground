bcd23dc02cf51bded7553724dc6384599763aca3
joaquinB <escusado@gmail.com>
2012-02-23 18:18:31 -0600

    added twitter bootstrap

--- js/toily-kinetic.js
+++ js/toily-kinetic.js
@@ -4,18 +4,22 @@
   var kinetic = {
 
     //Attributes
-    stage: new Kinetic.Stage("kinetic", 640, 480),
+    stage: new Kinetic.Stage("kinetic", $('#kinetic').width(), $('#kinetic').height()),
     date:  new Date(),
+    
+    //animation stop
     willStop: true,
+
     //Methods
     init: function(){
       var that = this;
+
       $('#stop').click(function(){
         that.willStop = false;
       });
 
       this.initialTime = this.date.getTime();
-      this.animate();
+      this.animation();
     },
     animation: function(){
       //update setup
@@ -30,8 +34,9 @@
 
       //update
       this.update();
+      
       //clear
-
+      
       //draw
 
       //animate loop
@@ -39,53 +44,10 @@
     },
     update: function(){
 
-    },
-    animate:function(){
-      var that = this;
-      var stage = this.stage;
-      var layer = this.layer;
-      var group = this.group;
-      
-      var currentDate = new Date();
-      var currentTime = currentDate.getTime();
-
-      var timeDiff = currentTime - this.initialTime;
-      $('#console').text(timeDiff);
-
-      if(triangle){
-        layer.remove(triangle);  
-      }
-
-      //update
-      var triangle = new Kinetic.Shape(function(){
-        var context = this.getContext();
-        context.beginPath();
-        context.lineWidth = 4;
-        context.strokeStyle = "black";
-        context.fillStyle = "#00D2FF";
-        context.moveTo(1+timeDiff, 50);
-        context.lineTo(250, 80);
-        context.lineTo(150, 170);
-        context.closePath();
-        context.fill();
-        context.stroke();
-      });
-
-      //clear
-      stage.clear();
-
-      // add the group to the layer
-      layer.add(triangle);
-
-      // add the layer to the stage
-      stage.add(layer);
-
-      if(this.willStop){
-        requestAnimFrame(function(){
-          that.animate();
-        });
-      }
     }
-  };
+
+  //----------------------  
+  }; //end kinetic Object
+
   kinetic.init();
 })();
